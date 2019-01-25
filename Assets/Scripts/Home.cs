839bece58db6e65dc0e47bb785cbab18a0093d24
Stephen Mills <s00132280@mail.itsligo.ie>
2019-01-25 20:21:38 +0000

    Added Home Script

---
+++ Assets/Scripts/Home.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Home : MonoBehaviour {
+
+    public string Colour;
+    public int Number;
+    public string Size;
+    public string Location;
+
+}
