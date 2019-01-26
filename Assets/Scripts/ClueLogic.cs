027dc76d4be590ad08a0ec8b1c8bea0d0ac1dbad
Philip D'Arcy <komeijiblue@gmail.com>
2019-01-26 17:22:30 +0000

    added camera

--- Assets/Scripts/ClueLogic.cs
+++ Assets/Scripts/ClueLogic.cs
@@ -52,6 +52,49 @@ public class ClueLogic : MonoBehaviour {
         {
             clue.transform.GetChild(0).gameObject.SetActive(true);
         }
+        else
+        {
+            if(type == 1)
+            {
+                possibleValues = GameObject.FindGameObjectsWithTag("Red");
+                foreach (GameObject clue in possibleValues)
+                {
+                    Color color = new Color(1,1,1);
+                    Debug.Log("uh oh");
+                    switch(value)
+                    {
+                        case "Blue":
+                            color = Color.blue;
+                            break;
+                        case "Green":
+                            color = Color.green;
+                            break;
+                        case "Yellow":
+                            color = Color.yellow;
+                            break;
+                        case "Black":
+                            color = Color.black;
+                            break;
+                        case "White":
+                            color = Color.white;
+                            break;
+                    }
+                    //Fetch the Renderer from the GameObject
+                    Renderer rend = clue.GetComponent<Renderer>();
+
+                    //Set the main Color of the Material to green
+                    rend.material.shader = Shader.Find("_Color");
+                    rend.material.SetColor("_Color", color);
+
+                    //Find the Specular shader and change its Color to red
+                    rend.material.shader = Shader.Find("Specular");
+                    rend.material.SetColor("_SpecColor", Color.black);
+                    clue.transform.GetChild(0).gameObject.SetActive(true);
+
+                    clue.tag = value;
+                }
+            }
+        }
         //GameObject clue = possibleValues[UnityEngine.Random.Range(0, possibleValues.Length)];
       
         
