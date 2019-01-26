e3f4b08ab5028e698806e96798f47f4e2d70e8e8
Philip D'Arcy <komeijiblue@gmail.com>
2019-01-26 11:47:24 +0000

    added colour changer and moved files to correct folders

---
+++ Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPicker : MonoBehaviour {
+
+    public Color[] _types;
+    // Use this for initialization
+    void Start()
+    {
+        int colorChoice = Random.Range(0, 21);
+
+        switch(colorChoice)
+        {
+            case 0:
+                this.gameObject.tag = "Red";
+            break;
+            case 3:
+                this.gameObject.tag = "Green";
+                break;
+            case 7:
+                this.gameObject.tag = "Blue";
+                break;
+            case 10:
+                this.gameObject.tag = "Yellow";
+                break;
+            case 14:
+                this.gameObject.tag = "White";
+                break;
+            case 18:
+                this.gameObject.tag = "Black";
+                break;
+            default:
+                this.gameObject.tag = "Color";
+                break;
+
+        }
+
+
+        //Fetch the Renderer from the GameObject
+        Renderer rend = GetComponent<Renderer>();
+
+        //Set the main Color of the Material to green
+        rend.material.shader = Shader.Find("_Color");
+        rend.material.SetColor("_Color", _types[colorChoice]);
+
+        //Find the Specular shader and change its Color to red
+        rend.material.shader = Shader.Find("Specular");
+        rend.material.SetColor("_SpecColor", Color.white);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
