b6b243e074beaac51325942ed677f96b55e987f4
Philip D'Arcy <komeijiblue@gmail.com>
2019-01-27 15:20:21 +0000

    added more homes

--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Home : MonoBehaviour {
     
-    public enum Color
+    public enum cColor
     {
         Red,
         Green,
@@ -13,7 +13,7 @@ public class Home : MonoBehaviour {
         White,
         Black
     };
-    public Color cSelector;
+    public cColor cSelector;
     public enum Size
     {
         House,
@@ -25,10 +25,46 @@ public class Home : MonoBehaviour {
     public string _size;
     public string Location;
     public GameObject hbeat;
+    public GameObject House;
 
     private void Start()
     {
         _colour = cSelector.ToString();
         _size = sSelector.ToString();
+
+        Color c = new Color(1, 1, 1);
+        switch (_colour)
+        {
+            case "Red":
+                c = Color.red;
+                break;
+            case "Blue":
+                c = Color.blue;
+                break;
+            case "Green":
+                c = Color.green;
+                break;
+            case "Yellow":
+                c = Color.yellow;
+                break;
+            case "Black":
+                c = Color.black;
+                break;
+            case "White":
+                c = Color.white;
+                break;
+        }
+
+
+        //Fetch the Renderer from the GameObject
+        Renderer rend = House.GetComponent<Renderer>();
+
+        //Set the main Color of the Material to green
+        rend.material.shader = Shader.Find("_Color");
+        rend.material.SetColor("_Color", c);
+
+        //Find the Specular shader and change its Color to red
+        rend.material.shader = Shader.Find("Specular");
+        rend.material.SetColor("_SpecColor", Color.black);
     }
 }
